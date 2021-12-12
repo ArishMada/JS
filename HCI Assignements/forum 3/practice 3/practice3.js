e2f90cd11a39abaf1dec1b00aeaff1f19197dd76
ArishMada <arishmadataly@gmail.com>
2021-12-12 12:25:38 +0300

    Update practice3.js

--- HCI Assignements/forum 3/practice 3/practice3.js
+++ HCI Assignements/forum 3/practice 3/practice3.js
@@ -1,7 +1,11 @@
 const climbingStairs = function(cost) {
-    for (let i = cost.length - 3; ~i; i--)
-        cost[i] += Math.min(cost[i+1], cost[i+2])
-    console.log(Math.min(cost[0], cost[1]));
+    let step1 = 0;
+    let step2 = 0;
+    for (let i = cost.length-1; i>=0; i--) {
+        let currentStep = cost[i] + Math.min(step1, step2);
+        step1 = step2;
+        step2 = currentStep;
+    }
+    console.log(Math.min(step1, step2));
 };
 
-climbingStairs([0, 0, 0])
